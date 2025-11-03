f48d9314b99857b12ec0b70ad127a421300f0fc8
Keven Fernandes <kevenpyw@gmail.com>
2025-11-03 02:45:10 -0300

    debug url vercel

--- src/lib/query-api.ts
+++ src/lib/query-api.ts
@@ -2,8 +2,10 @@ export async function getDataApi() {
   const vercelUrl = process.env.VERCEL_URL;
 
   const baseURL = vercelUrl ? `https://${vercelUrl}` : "http://localhost:3000";
+  console.log(baseURL);
 
   const res = await fetch(`${baseURL}/api/dados`);
+  console.log(res);
 
   if (!res.ok) {
     throw new Error("Erro ao buscar dados");
